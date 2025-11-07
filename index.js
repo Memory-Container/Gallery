e2e7bd95b8b2c89bbf0d95f5ac010763cfab6e89
Night Furry <ddhieu2707@gmail.com>
2025-11-07 22:21:45 +0700

    first-commit

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ let modal = document.querySelector(".modal")
 let card = document.querySelectorAll(".card")
 let header = document.querySelector(".name")
 let description = document.querySelector(".description")
-let desc = [
-  "I created this. It's kinda neat",
-  "Meadows. Kinda nice to be in one",
-  "Doggo, the love of my life and also is my profile picture",
-  "Look up at the sky, what can you see?",
-  "Minecraft Shader Sky (One of my project's background)",
-  "Unused background in some other game",
-  "The same profile picture, just flipped upside down",
-  "My favorite flower"
-]
 backdrop.addEventListener("click", (e) => {
     if (e.target === backdrop) {
       e.preventDefault()
@@ -29,9 +19,7 @@ card.forEach((cards, index) => {
   })
 })
 function OpenModal(index) {
-    header.textContent = text[index].textContent
-    image.style.backgroundImage = card[index].style.backgroundImage
-    description.textContent = "\"" + desc[index] + "\""
+    modal.style.backgroundImage = card[index].style.backgroundImage
     backdrop.style.display = "flex"
 }
 function CloseModal() {
